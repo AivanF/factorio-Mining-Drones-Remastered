71d39cccd54073794eb3d4b268792f35f8dd762a
Aivan <AivanF@users.noreply.github.com>
2023-06-08 15:17:23 +0300

    Translinguer update
    No actual change

--- scripts/locales.py
+++ scripts/locales.py
@@ -2,7 +2,7 @@ __author__ = 'AivanF'
 __contact__ = 'projects@aivanf.com'
 
 import re
-from translinguer import Translinguer
+from translinguer import Translinguer, dict_reversed
 
 base_path = '../Mining_Drones_Remastered'
 cfg_path = base_path + '/locale'
@@ -34,9 +34,9 @@ print(f'Extracted {len(SETTING_NAMES)} settings')
 
 
 def validate_settings(trans):
-    page = trans.texts['mining_drones']
-    section = page['mod-setting-name']
-    names_have = set(section.keys())
+    page = trans.pages['mining_drones']
+    section = page.sections['mod-setting-name']
+    names_have = set(section.entries.keys())
     names_need = SETTING_NAMES
     missed = names_need - names_have
     extra = names_have - names_need
@@ -50,25 +50,27 @@ def validate_settings(trans):
 def from_raw():
     # Used to create Google Sheets manually from raw files
     trans.load_cfg(cfg_path)
-    print(trans.to_csv())
+    trans.save_cache('test-cache.json')
+    # print(trans.to_csv())
 
 
 def from_remote():
     trans.load_from_gsheets(name='MD2R-Texts')
-    trans.stats()
+    print(trans.stats)
     trans.validate()
     trans.save_cache()
 
 
 def from_cache():
     trans.load_cache()
-    trans.stats()
+    print(trans.stats)
     validate_settings(trans)
     trans.validate(raise_error=True)
-    trans.save_cfg(cfg_path)
+    trans.save_cfg_by_language_page(cfg_path)
 
 
 if __name__ == '__main__':
-    from_remote()
+    # from_raw()
+    # from_remote()
     from_cache()
     pass
